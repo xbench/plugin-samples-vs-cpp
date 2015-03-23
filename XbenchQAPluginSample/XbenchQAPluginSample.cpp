a85587883798a609fe5af6cefd911c4d45c207da
Pep Condal <pcondal@apsic.com>
2015-03-23 17:35:29 +0100

    Changed tab/spaces/indentation conventions

--- XbenchQAPluginSample/XbenchQAPluginSample.cpp
+++ XbenchQAPluginSample/XbenchQAPluginSample.cpp
@@ -7,8 +7,8 @@
 //---------------------------------------------------------------------------//
 
 typedef struct _TLongestResult {
-	size_t length;
-	DWORD segment_id;
+  size_t length;
+  DWORD segment_id;
 } TLongestResult;
 
 //---------------------------------------------------------------------------//
@@ -39,89 +39,83 @@ DWORD g_longest_index;
 // or viceversa, return the error message, otherwise (no error), return NULL //
 //---------------------------------------------------------------------------//
 wchar_t * CheckSuspiciousLength(const size_t aSourceLength,
-								const size_t aTargetLength)
-{
-	if (aSourceLength > 60) {
-		if (aTargetLength > aSourceLength * 1.8)
-			return L"Target 80% larger than Source";
-	} else if (aSourceLength > 45) {
-		if (aTargetLength > aSourceLength * 2.2)
-			return L"Target 120% larger than Source";
-	} else if (aSourceLength > 30) {
-		if (aTargetLength > aSourceLength * 2.6)
-			return L"Target 160% larger than Source";
-	} else if (aSourceLength > 15) {
-		if (aTargetLength > aSourceLength * 3)
-			return L"Target 200% larger than Source";
-	}
+                                const size_t aTargetLength) {
+  if (aSourceLength > 60) {
+    if (aTargetLength > aSourceLength * 1.8)
+      return L"Target 80% larger than Source";
+  } else if (aSourceLength > 45) {
+    if (aTargetLength > aSourceLength * 2.2)
+      return L"Target 120% larger than Source";
+  } else if (aSourceLength > 30) {
+    if (aTargetLength > aSourceLength * 2.6)
+      return L"Target 160% larger than Source";
+  } else if (aSourceLength > 15) {
+    if (aTargetLength > aSourceLength * 3)
+      return L"Target 200% larger than Source";
+  }
 
-	if (aTargetLength > 60)	{
-		if (aSourceLength > aTargetLength * 1.8)
-			return L"Source 80% larger than Target";
-	} else if (aTargetLength > 45) {
-		if (aSourceLength > aTargetLength * 2.2)
-			return L"Source 120% larger than Target";
-	} else if (aTargetLength > 30) {
-		if (aSourceLength > aTargetLength * 2.6)
-			return L"Source 160% larger than Target";
-	} else if (aTargetLength > 15) {
-		if (aSourceLength > aTargetLength * 3)
-			return L"Source 200% larger than Target";
-	}
+  if (aTargetLength > 60)	{
+    if (aSourceLength > aTargetLength * 1.8)
+      return L"Source 80% larger than Target";
+  } else if (aTargetLength > 45) {
+    if (aSourceLength > aTargetLength * 2.2)
+      return L"Source 120% larger than Target";
+  } else if (aTargetLength > 30) {
+    if (aSourceLength > aTargetLength * 2.6)
+      return L"Source 160% larger than Target";
+  } else if (aTargetLength > 15) {
+  	if (aSourceLength > aTargetLength * 3)
+      return L"Source 200% larger than Target";
+  }
 
-	return NULL;
+  return NULL;
 }
 
 //---------------------------------------------------------------------------//
 // Check longest. Keep a global array with the longest target segments.      //
 //---------------------------------------------------------------------------//
-void CheckLongest(const size_t aTargetLength, const DWORD aSegId)
-{
-	if (aTargetLength > g_longest[kLongestCount - 1].length)
-	{
-		g_longest[kLongestCount - 1].length = aTargetLength;
-		g_longest[kLongestCount - 1].segment_id = aSegId;
+void CheckLongest(const size_t aTargetLength, const DWORD aSegId) {
+  if (aTargetLength > g_longest[kLongestCount - 1].length) {
+    g_longest[kLongestCount - 1].length = aTargetLength;
+    g_longest[kLongestCount - 1].segment_id = aSegId;
 
-		for (int i = kLongestCount - 2;
-			 (i >= 0) && (g_longest[i + 1].length > g_longest[i].length);
-			 i--)
-		{
-			TLongestResult aux = g_longest[i];
-			g_longest[i] = g_longest[i + 1];
-			g_longest[i + 1] = aux;
-		}
-	}
+    for (int i = kLongestCount - 2;
+         (i >= 0) && (g_longest[i + 1].length > g_longest[i].length);
+         i--) {
+      TLongestResult aux = g_longest[i];
+      g_longest[i] = g_longest[i + 1];
+      g_longest[i + 1] = aux;
+    }
+  }
 }
 
 //---------------------------------------------------------------------------//
 // GetDeclareInfo. Callback called once at DLL load to provide information   //
 // about the DLL.                                                            //
 //---------------------------------------------------------------------------//
-XBENCHQA_API PQAPluginDeclareInfo __stdcall GetDeclareInfo(void)
-{
-	static TQAPluginDeclareInfo plugin_declare_info;
+XBENCHQA_API PQAPluginDeclareInfo __stdcall GetDeclareInfo(void) {
+  static TQAPluginDeclareInfo plugin_declare_info;
 
-	plugin_declare_info.StructVersion = 0;
+  plugin_declare_info.StructVersion = 0;
 
-	wcscpy_s(plugin_declare_info.Description, L"Sample plugin for Xbench");
-	wcscpy_s(plugin_declare_info.Keywords, L"Sample, Example, Plugin");
+  wcscpy_s(plugin_declare_info.Description, L"Sample plugin for Xbench");
+  wcscpy_s(plugin_declare_info.Keywords, L"Sample, Example, Plugin");
 
-	plugin_declare_info.Version = 0x0100;
-	plugin_declare_info.SourceLang = 0;
-	plugin_declare_info.TargetLang = 0;
-	plugin_declare_info.AllowsUnicode = true;
-	plugin_declare_info.AllowsAnsi = true;
+  plugin_declare_info.Version = 0x0100;
+  plugin_declare_info.SourceLang = 0;
+  plugin_declare_info.TargetLang = 0;
+  plugin_declare_info.AllowsUnicode = true;
+  plugin_declare_info.AllowsAnsi = true;
 
-	return &plugin_declare_info;
+  return &plugin_declare_info;
 }
 
 //---------------------------------------------------------------------------//
 // GetDeclareName.  Callback called once at DLL load.  The name specified    //
 // here appears in the Group Check list box of Xbench QA tab                 //
 //---------------------------------------------------------------------------//
-XBENCHQA_API PWSTR __stdcall GetDeclareName(void)
-{
-	return L"Sample Plugin";
+XBENCHQA_API PWSTR __stdcall GetDeclareName(void) {
+  return L"Sample Plugin";
 }
 
 //---------------------------------------------------------------------------//
@@ -131,41 +125,36 @@ XBENCHQA_API PWSTR __stdcall GetDeclareName(void)
 //                                                                           //
 // For this example we have two functions, so it will show two strings.      //
 //---------------------------------------------------------------------------//
-XBENCHQA_API PSTR __stdcall GetFunctionName(DWORD aHandle)
-{
-	switch (aHandle)
-	{
-		case kCheckSuspiciousHandle: return "Suspicious length";
-		case kCheckLongestHandle: return "Show 3 longest";
-		default: return "";
-	}
+XBENCHQA_API PSTR __stdcall GetFunctionName(DWORD aHandle) {
+  switch (aHandle) {
+    case kCheckSuspiciousHandle: return "Suspicious length";
+    case kCheckLongestHandle: return "Show 3 longest";
+    default: return "";
+  } 
 }
 
 //---------------------------------------------------------------------------//
 // GetFirstFunction.  Callback called once at DLL load.  It returns the      //
 // first handle of the exposed QA functions, typically 1.                    //
 //---------------------------------------------------------------------------//
-XBENCHQA_API DWORD __stdcall GetFirstFunction(void)
-{
-	return g_function_index = 1;
+XBENCHQA_API DWORD __stdcall GetFirstFunction(void) {
+  return g_function_index = 1;
 }
 
 //---------------------------------------------------------------------------//
 // GetNextFunction. Returns the next handle for a QA function.  Returns 0 if //
 // there are no more handles to expose.                                      //
 //---------------------------------------------------------------------------//
-XBENCHQA_API DWORD __stdcall GetNextFunction(void)
-{
-	return (++g_function_index > kFunctionCount) ? 0 : g_function_index;
+XBENCHQA_API DWORD __stdcall GetNextFunction(void) {
+  return (++g_function_index > kFunctionCount) ? 0 : g_function_index;
 }
 
 //---------------------------------------------------------------------------//
 // SetConfigFile. This function is not currently called by Xbench but must   //
 // be exported in the DLL.                                                   //
 //---------------------------------------------------------------------------//
-XBENCHQA_API void __stdcall SetConfigFile(PSTR aFile)
-{
-	return;
+XBENCHQA_API void __stdcall SetConfigFile(PSTR aFile) {
+  return;
 }
 
 //---------------------------------------------------------------------------//
@@ -175,14 +164,11 @@ XBENCHQA_API void __stdcall SetConfigFile(PSTR aFile)
 // thread, a dialog to specify process data may be included there.           //
 //---------------------------------------------------------------------------//
 XBENCHQA_API void __stdcall ProcessBegin(DWORD aHandle,
-										 PQAFunctionParams aParams)
-{
-  if (aHandle == kCheckLongestHandle)
-  {
-    for (int i = 0; i < 3; i++)
-    {
-		g_longest[i].length = 0;
-		g_longest[i].segment_id = NULL;
+	                                     PQAFunctionParams aParams) {
+  if (aHandle == kCheckLongestHandle) {
+    for (int i = 0; i < 3; i++) {
+      g_longest[i].length = 0;
+      g_longest[i].segment_id = NULL;
     }
   }
 }
@@ -191,9 +177,8 @@ XBENCHQA_API void __stdcall ProcessBegin(DWORD aHandle,
 // ProcessEnd. Callback called upon completion of the QA. Here you can free  //
 // and unassign anything you previously initialized.                         //
 //---------------------------------------------------------------------------//
-XBENCHQA_API void __stdcall ProcessEnd(DWORD aHandle)
-{
-	return;
+XBENCHQA_API void __stdcall ProcessEnd(DWORD aHandle) {
+  return;
 }
 
 //---------------------------------------------------------------------------//
@@ -208,63 +193,57 @@ XBENCHQA_API void __stdcall ProcessEnd(DWORD aHandle)
 // three longest ones.                                                       //
 //---------------------------------------------------------------------------//
 XBENCHQA_API PQAPluginResult __stdcall ProcessSegment(DWORD aHandle,
-													  TQASegmentInfo aSegInfo)
-{
-	// If no Wide string is available, we'll use the Ansi string
-	size_t source_length = (aSegInfo.WSource) ? wcslen(aSegInfo.WSource)
-		                                      : strlen(aSegInfo.Source);
-	size_t target_length = (aSegInfo.WTarget) ? wcslen(aSegInfo.WTarget)
-		                                      : strlen(aSegInfo.Target);
+                                                      TQASegmentInfo aSegInfo) {
+  // If no Wide string is available, we'll use the Ansi string
+  size_t source_length = (aSegInfo.WSource) ? wcslen(aSegInfo.WSource)
+                                            : strlen(aSegInfo.Source);
+  size_t target_length = (aSegInfo.WTarget) ? wcslen(aSegInfo.WTarget)
+                                            : strlen(aSegInfo.Target);
 
-	switch (aHandle)
-	{
-		case kCheckSuspiciousHandle:
-		{
-			wchar_t * result_text = CheckSuspiciousLength(source_length,
-				                                          target_length);
-			if (result_text)
-			{
-				static TQAPluginResult result;
-				result.Text = result_text;
-				result.SegId = aSegInfo.SegId;
-				result.Options = NULL;
-				return &result;
-			}
-			else
-				return NULL;
-		}
-		case kCheckLongestHandle:
-		    return CheckLongest(target_length, aSegInfo.SegId), NULL;
-		default:
-			return NULL;
-	}
+  switch (aHandle) {
+    case kCheckSuspiciousHandle: {
+      wchar_t * result_text = CheckSuspiciousLength(source_length,
+                                                    target_length);
+      if (result_text) {
+        static TQAPluginResult result;
+        result.Text = result_text;
+        result.SegId = aSegInfo.SegId;
+        result.Options = NULL;
+        return &result;
+      } else
+        return NULL;
+    }
+    case kCheckLongestHandle:
+      CheckLongest(target_length, aSegInfo.SegId);
+      default:
+        return NULL;
+  }
 }
 
-PQAPluginResult ReadLongestItemsResult(void)
-{
-	if ((g_longest_index > kLongestCount - 1) ||
-		(g_longest[g_longest_index].segment_id == NULL))
-		return NULL;
+PQAPluginResult ReadLongestItemsResult(void) {
+  if ((g_longest_index > kLongestCount - 1) ||
+  	  (g_longest[g_longest_index].segment_id == NULL))
+    return NULL;
 
-	static TQAPluginResult result;
+  static TQAPluginResult result;
 
-	const int kBufferSize = 20;
-	static wchar_t text_result[kBufferSize];
-	result.Text = &text_result[0];
-	swprintf(result.Text,
-		     kBufferSize - 1,
-			 L"Length: %d", g_longest[g_longest_index].length);
-	result.SegId = g_longest[g_longest_index].segment_id;
+  const int kBufferSize = 20;
+  static wchar_t text_result[kBufferSize];
+  result.Text = &text_result[0];
+  swprintf(result.Text,
+           kBufferSize - 1,
+           L"Length: %d", g_longest[g_longest_index].length);
+  result.SegId = g_longest[g_longest_index].segment_id;
 
-	static TQAPluginResultOptions result_options;
-	result.Options = &result_options;
-	result.Options->StructVersion = 0;
-	result.Options->Sorted = true;
-	result.Options->Groupable = true;
+  static TQAPluginResultOptions result_options;
+  result.Options = &result_options;
+  result.Options->StructVersion = 0;
+  result.Options->Sorted = true;
+  result.Options->Groupable = true;
 
-	g_longest_index++;
+  g_longest_index++;
 
-	return &result;
+  return &result;
 }
 
 //---------------------------------------------------------------------------//
@@ -278,14 +257,13 @@ PQAPluginResult ReadLongestItemsResult(void)
 // insert a dialog that deals with results before they are actually returned //
 // to Xbench (this is how the Xbench spell-checkers work).                   //
 //---------------------------------------------------------------------------//
-XBENCHQA_API PQAPluginResult __stdcall GetFirstResult(DWORD aHandle)
-{
-	if (aHandle == kCheckLongestHandle) {
-		g_longest_index = 0;
-		return ReadLongestItemsResult();
-	}
+XBENCHQA_API PQAPluginResult __stdcall GetFirstResult(DWORD aHandle) {
+  if (aHandle == kCheckLongestHandle) {
+    g_longest_index = 0;
+    return ReadLongestItemsResult();
+  }
 
-	return NULL;
+  return NULL;
 }
 
 //---------------------------------------------------------------------------//
@@ -293,10 +271,9 @@ XBENCHQA_API PQAPluginResult __stdcall GetFirstResult(DWORD aHandle)
 // until NULL is returned to indicate that there are no more results to add  //
 // to the Xbench QA results UI.                                              //
 //---------------------------------------------------------------------------//
-XBENCHQA_API PQAPluginResult __stdcall GetNextResult(DWORD aHandle)
-{
-	if (aHandle == kCheckLongestHandle)
-		return ReadLongestItemsResult();
+XBENCHQA_API PQAPluginResult __stdcall GetNextResult(DWORD aHandle) {
+  if (aHandle == kCheckLongestHandle)
+    return ReadLongestItemsResult();
 
-	return NULL;
+  return NULL;
 }
--- XbenchQAPluginSample/XbenchQAPluginSample.h
+++ XbenchQAPluginSample/XbenchQAPluginSample.h
@@ -9,50 +9,50 @@
 #endif
 
 typedef struct _TQAPluginResultOptions {
-	WORD StructVersion;
-	BOOL Sorted;
-	BOOL Groupable;
+  WORD StructVersion;
+  BOOL Sorted;
+  BOOL Groupable;
 } TQAPluginResultOptions, *PQAPluginResultOptions;
 
 typedef struct _TQAPluginResult {
-	PWSTR Text;
-	DWORD SegId;
-	PQAPluginResultOptions Options;
+  PWSTR Text;
+  DWORD SegId;
+  PQAPluginResultOptions Options;
 } TQAPluginResult, *PQAPluginResult;
 
 typedef struct _TQAPluginDeclareInfo {
-	WORD StructVersion;
-	WCHAR Description[256];
-	WCHAR Keywords[256];
-	WORD Version;
-	LANGID SourceLang;
-	LANGID TargetLang;
-	BOOL AllowsUnicode;
-	BOOL AllowsAnsi;
+  WORD StructVersion;
+  WCHAR Description[256];
+  WCHAR Keywords[256];
+  WORD Version;
+  LANGID SourceLang;
+  LANGID TargetLang;
+  BOOL AllowsUnicode;
+  BOOL AllowsAnsi;
 } TQAPluginDeclareInfo, *PQAPluginDeclareInfo;
 
 typedef struct _TQASegmentInfo {
-	PSTR Source;
-	PSTR Target;
-	PWSTR WSource;
-	PWSTR WTarget;
-	DWORD SegId;
+  PSTR Source;
+  PSTR Target;
+  PWSTR WSource;
+  PWSTR WTarget;
+  DWORD SegId;
 } TQASegmentInfo, *PQASegmentInfo;
 
 typedef void *PQAFunctionParams;
 
 extern "C" {
-	XBENCHQA_API PWSTR __stdcall GetDeclareName(void);
-	XBENCHQA_API DWORD __stdcall GetFirstFunction(void);
-	XBENCHQA_API DWORD __stdcall GetNextFunction(void);
-	XBENCHQA_API PQAPluginDeclareInfo __stdcall GetDeclareInfo(void);
-	XBENCHQA_API void _stdcall SetConfigFile(PSTR aFile);
-	XBENCHQA_API void _stdcall ProcessBegin(DWORD aHandle, 
-		                                    PQAFunctionParams aParams);
-	XBENCHQA_API void _stdcall ProcessEnd(DWORD aHandle);
-	XBENCHQA_API PQAPluginResult _stdcall ProcessSegment(DWORD aHandle, 
-		                                                 TQASegmentInfo aSegInfo);
-	XBENCHQA_API PQAPluginResult _stdcall GetFirstResult(DWORD aHandle);
-	XBENCHQA_API PQAPluginResult _stdcall GetNextResult(DWORD aHandle);
-	XBENCHQA_API PSTR _stdcall GetFunctionName(DWORD aHandle);
+  XBENCHQA_API PWSTR __stdcall GetDeclareName(void);
+  XBENCHQA_API DWORD __stdcall GetFirstFunction(void);
+  XBENCHQA_API DWORD __stdcall GetNextFunction(void);
+  XBENCHQA_API PQAPluginDeclareInfo __stdcall GetDeclareInfo(void);
+  XBENCHQA_API void _stdcall SetConfigFile(PSTR aFile);
+  XBENCHQA_API void _stdcall ProcessBegin(DWORD aHandle, 
+                                          PQAFunctionParams aParams);
+  XBENCHQA_API void _stdcall ProcessEnd(DWORD aHandle);
+  XBENCHQA_API PQAPluginResult _stdcall ProcessSegment(DWORD aHandle, 
+                                                       TQASegmentInfo aSegInfo);
+  XBENCHQA_API PQAPluginResult _stdcall GetFirstResult(DWORD aHandle);
+  XBENCHQA_API PQAPluginResult _stdcall GetNextResult(DWORD aHandle);
+  XBENCHQA_API PSTR _stdcall GetFunctionName(DWORD aHandle);
 }
